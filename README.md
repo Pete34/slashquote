8c06fae44c292073c3f64407e8d9282645d069ec
Peter Grady <notes4pete@gmail.com>
2019-08-27 15:05:45 -0500

    added random pics

---
+++ README.md
@@ -0,0 +1,1 @@
+# **Slash Command For Slack**
--- index.js
+++ index.js
@@ -7,43 +7,57 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/public", express.static(path.resolve(__dirname, 'public')));
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
 app.post('/', (req, res) => {
     console.log(req);
     // implement your bot here ...
 
-
-    // 1. validate secret 
-    // https://api.slack.com/docs/verifying-requests-from-slack
-
-    // 2. potentially store in db any info.
-    // team_id and user_id together are unique.
-
-    const response_url = req.response_url;
-    console.log(response_url);
-
-
-    let data = {
-        response_type: 'in_channel', // public to channel.
-        text: 'Quote should be here..',
-        attachments: [
-            {
-                image_url: 'https://fb8797d1.ngrok.io/public/PageNotFound.png'
-            }
-        ]
-    }
-
     async function getQuote() {
         try {
-            const response = await axios.get('https://quotesapi.hypothetical.space/api/RNGQuote');
+
+            // get image data
+            const giphy = await axios.get(
+                'https://api.giphy.com/v1/gifs/random?api_key=wOi3i1UvK6w5fBoRtQ1vfjRucYgu4HRF&tag=cat&rating=PG'
+            );
+
+            // grab image url
+            const image = giphy.data.data.images.fixed_width.url;
+
+            // get quote data
+            const response = await axios.get(
+                'https://quotesapi.hypothetical.space/api/RNGQuote'
+            );
+
+            // map quote data.
             let quote = response.data.content;
             let source = response.data.source;
             let url = response.data.sourceUrl;
-            let text = `${quote}\n${source}\n${url}`
-            data.text = text;
-            res.json(data);
 
+            // build into text string
+            let text = `\n *Quote from Celia's API*:smile:\n\n${quote}\n${source}\n${url}\n`;
+
+
+            let data = {
+                response_type: 'in_channel', // public to channel.
+
+                text: text,
+                mrkdwn: true,
+                attachments: [
+                    {
+                        image_url: image
+                    }
+                ]
+            };
+
+            // return data object.
+            res.json(data);
         } catch (error) {
             console.error(error);
             res.json(data);
@@ -51,10 +65,12 @@ app.post('/', (req, res) => {
     }
 
     getQuote();
-
- 
 });
 
 const server = app.listen(3000, () => {
-    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+    console.log(
+        'Express server listening on port %d in %s mode',
+        server.address().port,
+        app.settings.env
+    );
 });
